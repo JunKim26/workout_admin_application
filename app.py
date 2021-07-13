171a6f553e1706a166136e9142d789aeeb3c87a8
Michael B. Stearns <michaelbstearns@gmail.com>
2021-07-12 22:01:05 -0700

    First commit with starter Flask app and jinja2

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import os
+
+# Configuration
+
+app = Flask(__name__)
+
+# Routes
+
+@app.route('/')
+def root():
+    return render_template("main.j2")
+
+# Listener
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 9112))
+    app.run(port=port, debug=True)
