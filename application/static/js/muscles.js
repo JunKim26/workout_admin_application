8b172881ae1ef639987d330cccf2ac5e2572d9f6
Michael B. Stearns <michaelbstearns@gmail.com>
2021-09-10 14:53:23 -0700

    Updated Muscles html and JS to use common.mjs module.

--- application/static/js/muscles.js
+++ application/static/js/muscles.js
@@ -1,40 +1,47 @@
+import {
+  createTable,
+  clearFormInputs,
+  updateFormInputValuesListener,
+  updateTableDataInputValuesListener,
+  editButtonListener,
+  updateButtonListener,
+  deleteButtonListener,
+  destroyAndRecreateTable,
+} from './modules/common.mjs';
+
 const MUSCLES_API_URL = "http://127.0.0.1:5000/muscles-api";
 const TABLE_ID = "#table";
 
 const dataAttributes = {
-    "muscle_name": {
-        "type": "text",
-        "name": "muscle_name"
-    },
-    "muscle_group_name": {
-      "type": "text",
-      "name": "muscle_group_name"
-    }
-}
+  "muscle_name": {
+    "type": "text",
+    "name": "muscle_name"
+  },
+  "muscle_group_name": {
+    "type": "text",
+    "name": "muscle_group_name"
+  }
+};
 
 const getData = async () => {
-    try {
-        const res = await fetch(
-        //"http://flip1.engr.oregonstate.edu:3319/",
-        MUSCLES_API_URL,
-        {
-            method: "GET"
-        }
-        );
-        const data = await res.json();
-        return data;
-    } catch (e) {
-        let msg = "GET error!";
-        console.log(msg);
-        console.log(e);
-    }
-  };
+  try {
+      const res = await fetch(
+      //"http://flip1.engr.oregonstate.edu:3319/",
+      MUSCLES_API_URL,
+      {
+          method: "GET"
+      }
+      );
+      const data = await res.json();
+      return data;
+  } catch (e) {
+      let msg = "GET error!";
+      console.log(msg);
+      console.log(e);
+  }
+};
 
 const addRow = async (data) => {
-  console.log("---- look here!!! ------")
-  console.log(data);
-  console.log(data.muscle_name);
-  console.log(data.muscle_group);
   try {
     const res = await fetch(
       //"http://flip1.engr.oregonstate.edu:3319/",
@@ -57,7 +64,7 @@ const addRow = async (data) => {
     console.log(e);
     return false;
   }
-}
+};
 
 const updateData = async (data) => {
   console.log(data);
@@ -107,6 +114,13 @@ const deleteRow = async (rowID) => {
   }
 };
 
+function apiCalls() {
+  this.getData = getData;
+  this.addRow = addRow;
+  this.updateData = updateData;
+  this.deleteRow = deleteRow;
+};
+
 function updateButtonAPICall(tdElementsArray) {
   let data = {
     muscle_id: tdElementsArray[0].innerText,
@@ -118,12 +132,13 @@ function updateButtonAPICall(tdElementsArray) {
   response.then(success => {
     if (success) {
       clearFormInputs();
-      destroyAndRecreateTable();
+      let apiCallsObj = new apiCalls();
+      destroyAndRecreateTable(apiCallsObj, createTable, dataAttributes);
     }
   });
 };
 
-function addButtonSendAPICall() {
+function addButtonSendAPICall(apiCallsObj, createTable, dataAttributes) {
   let addButton = document.querySelector("#add_button");
   addButton.addEventListener("click", () => {
     let formElement = addButton.parentElement.parentElement;
@@ -139,204 +154,37 @@ function addButtonSendAPICall() {
       if (success) {
         // clear data in form after Add button clicked
         clearFormInputs();
-        destroyAndRecreateTable();
+        destroyAndRecreateTable(apiCallsObj, createTable, dataAttributes);
       }
     });
   });
 };
 
-
-function createTable(dataRowsArray) {
-  let tableElement = document.querySelector("#table");
-  let tableBodyElement = document.createElement("tbody");
-  tableBodyElement.setAttribute("id", "table_body");
-  tableElement.appendChild(tableBodyElement);
-
-  for (let row = 0; row < dataRowsArray.length; row++) {
-    let tableRowElement = document.createElement("tr");
-    tableBodyElement.appendChild(tableRowElement)
-
-    let jsonDataRow = dataRowsArray[row];
-    let keysArray = Object.keys(jsonDataRow);
-
-    // create id table data
-    let idTDElement = document.createElement("td");
-    idTDElement.innerText = jsonDataRow.muscle_id;
-    idTDElement.setAttribute("class", "db_id");
-    tableRowElement.appendChild(idTDElement);
-
-    // populate table with muscle data
-    for (let index = 1; index < keysArray.length; index++) {
-      let tdElement = document.createElement("td");
-      let inputElement = document.createElement("input");
-      let key = keysArray[index];
-      inputElement.setAttribute("type", dataAttributes[key].type);
-      inputElement.setAttribute("name", dataAttributes[key].name);
-      inputElement.setAttribute("value", jsonDataRow[key]);
-      inputElement.disabled = true;
-      tdElement.appendChild(inputElement);
-      tableRowElement.appendChild(tdElement);
-    }
-
-    // add edit button
-    let updateButton = document.createElement("button");
-    updateButton.setAttribute("type", "button");
-    updateButton.setAttribute("class", "edit-button");
-    updateButton.setAttribute("id", `edit-button-${row}`);
-    updateButton.innerHTML = "Edit";
-    tdElement = document.createElement("td");
-    tdElement.appendChild(updateButton);
-    tableRowElement.appendChild(tdElement);
-
-    // add delete button
-    let deleteButton = document.createElement("button");
-    deleteButton.setAttribute("type", "button");
-    deleteButton.setAttribute("class", "delete-button");
-    deleteButton.setAttribute("id", `delete-button-${row}`);
-    deleteButton.innerHTML = "Delete";
-    tdElement = document.createElement("td");
-    tdElement.appendChild(deleteButton);
-    tableRowElement.appendChild(tdElement);
-  }
-}
-
-function destroyAndRecreateTable() {
-  // destroy table
-  let tableBodyElement = document.querySelector("#table_body");
-  tableBodyElement.remove();
-  // create brand new table (entire table)
-  let responseData = getData();
-  responseData.then(dataRowsArray => {
-    createTable(dataRowsArray);
-  });
-}
-
-function checkRowForUpdates(childTDElements) {
-  let updates = false;
-  for (let rowIndex = 1; rowIndex < childTDElements.length - 2; rowIndex++) {
-    let classStr = childTDElements[rowIndex].firstChild.getAttribute("class");
-    if (classStr) {
-      if (classStr.includes("field-updated")) {
-        updates = true;
-        break;
-      }
-    }
-  }
-  return updates;
-}
-
-function deleteButtonListener() {
-  let tableElement = document.querySelector("#table");
-  
-  tableElement.addEventListener("click", event => {
-    let targetElement = event.target;
-
-    // update button behavior
-    let buttonClass = targetElement.getAttribute("class");
-    if(buttonClass === "delete-button") {
-      let trElement = targetElement.parentElement.parentElement;
-      let databaseID = trElement.firstChild.innerText;
-      
-      // send delete command to node server via API
-      let response = deleteRow(databaseID);
-      response.then(success => {
-        if (success) {
-            destroyAndRecreateTable();
-        }
-      });
-    }
-  });
-}
-
-function updateButtonListener() {
-  let tableElement = document.querySelector("#table");
-  
-  tableElement.addEventListener("click", event => {
-    let targetElement = event.target;
-
-    // update button behavior
-    let buttonClass = targetElement.getAttribute("class");
-    if(buttonClass === "update-button") {
-      let trElement = targetElement.parentElement.parentElement;
-      let childTDElements = trElement.children;
-
-      // check if anything was changed before starting update process
-      let updates = checkRowForUpdates(childTDElements);
-      if (updates) {
-        // update API call here
-        updateButtonAPICall(childTDElements);
-      }
-    }
-  });
-}
-
-function editButtonListener() {
-  let tableElement = document.querySelector("#table");
-  
-  // undisable row when muscle clicks Edit button
-  tableElement.addEventListener("click", event => {
-    let targetElement = event.target;
-    let buttonClass = targetElement.getAttribute("class");
-    if(buttonClass === "edit-button") {
-      let trElement = targetElement.parentElement.parentElement;
-      let childTDElements = trElement.children;
-
-      for (let rowIndex = 1; rowIndex < childTDElements.length - 2; rowIndex++) {
-        childTDElements[rowIndex].firstChild.disabled = false;
-      }
-      targetElement.innerText = "Update";
-      targetElement.className = "update-button";
-      }
-  });
-}
-
-function updateFormInputValues() {
-  let formElement = document.querySelector("#form");
-  formElement.addEventListener("input", event => {
-    event.target.setAttribute("value", event.target.value);
-  });
-}
-
-function updateTableDataInputValues() {
-  let tableElement = document.querySelector("#table");
-  tableElement.addEventListener("input", event => {
-    event.target.setAttribute("value", event.target.value);
-    let classes = event.target.classList;
-    classes.add("field-updated");
-  });
-}
-
 function createEventListeners() {
+  let apiCallsObj = new apiCalls();
   // update 'value' attribute when muscle changes input in form
-  updateFormInputValues();
+  updateFormInputValuesListener();
   
   // update 'value' attribute when muscle changes input in table
-  updateTableDataInputValues();
+  updateTableDataInputValuesListener(TABLE_ID);
 
   // event listeners for clicks on update and delete buttons
   //createButtonListeners();
   editButtonListener();
-  updateButtonListener();
-  deleteButtonListener();
+  updateButtonListener(updateButtonAPICall);
+  deleteButtonListener(apiCallsObj, createTable, dataAttributes);
 
   // event listener for adding new user via form
-  addButtonSendAPICall();
-}
-
-function clearFormInputs() {
-  let formElement = document.querySelector("#form");
-  for (let index = 0; index < formElement.length - 1; index++) {
-    formElement[index].value = "";
-  }
-}
+  addButtonSendAPICall(apiCallsObj, createTable, dataAttributes);
+};
 
 function main() {
   let responseData = getData();
   responseData.then(dataRowsArray => {
-    createTable(dataRowsArray);
+    createTable(dataRowsArray, dataAttributes);
   });
   clearFormInputs();
   createEventListeners();
-}
+};
 
 main();
