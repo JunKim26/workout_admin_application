6959893e4da854807d79b27686f2c419246ae9f1
Michael B. Stearns <michaelbstearns@gmail.com>
2021-09-11 16:31:35 -0700

    Updated the MuscleGroups-Exercises pages to use the new common.mjs module.

--- application/static/js/musclegroups_exercises.js
+++ application/static/js/musclegroups_exercises.js
@@ -1,4 +1,14 @@
-const MUSCLE_GROUP_EXERCISES_API_URL = "http://127.0.0.1:5000/muscle-groups-exercises-api"
+import {
+  createManyToManyTable,
+  completeExercisesSelectOptions,
+  resetSelectElement,
+  updateTableDataInputValuesListener,
+  deleteButtonListenerManyToMany,
+  destroyAndRecreateTable,
+} from './modules/common.mjs';
+
+const MUSCLE_GROUP_EXERCISES_API_URL = "http://127.0.0.1:5000/muscle-groups-exercises-api";
+const TABLE_ID = "#many-to-many-table";
 
 const dataAttributes = {
   "muscle_group_id": {
@@ -103,8 +113,6 @@ const updateData = async (data) => {
 };
 
 const deleteRow = async (muscle_group_id, exercise_id) => {
-  console.log("Look here!!!!!");
-  console.log(exercise_id);
   try {
     const res = await fetch(
       MUSCLE_GROUP_EXERCISES_API_URL,
@@ -162,159 +170,16 @@ const getExercisesData = async () => {
   }
 };
 
-function createTable(dataRowsArray) {
-  let tableElement = document.querySelector("#many-to-many-table");
-  let tableBodyElement = document.createElement("tbody");
-  tableBodyElement.setAttribute("id", "table_body");
-  tableElement.appendChild(tableBodyElement);
-
-  for (let row = 0; row < dataRowsArray.length; row++) {
-    let tableRowElement = document.createElement("tr");
-    tableBodyElement.appendChild(tableRowElement)
-
-    let jsonDataRow = dataRowsArray[row];
-    let keysArray = Object.keys(jsonDataRow);
-
-    // create muscle groupd id table data
-    let idTDElement = document.createElement("td");
-    idTDElement.innerText = jsonDataRow.muscle_group_id;
-    idTDElement.setAttribute("class", "db_id");
-    tableRowElement.appendChild(idTDElement);
-    
-    // create exercise id table data
-    idTDElement = document.createElement("td");
-    idTDElement.innerText = jsonDataRow.exercise_id;
-    idTDElement.setAttribute("class", "db_id");
-    tableRowElement.appendChild(idTDElement);
-
-    // populate table with data
-    for (let index = 2; index < keysArray.length; index++) {
-      let tdElement = document.createElement("td");
-      let inputElement = document.createElement("input");
-      let key = keysArray[index];
-      inputElement.setAttribute("type", dataAttributes[key].type);
-      inputElement.setAttribute("name", dataAttributes[key].name);
-      inputElement.setAttribute("value", jsonDataRow[key]);
-      inputElement.disabled = true;
-      tdElement.appendChild(inputElement);
-      tableRowElement.appendChild(tdElement);
-    }
-
-    // add delete button
-    let deleteButton = document.createElement("button");
-    deleteButton.setAttribute("type", "button");
-    deleteButton.setAttribute("class", "delete-button");
-    deleteButton.setAttribute("id", `delete-button-${row}`);
-    deleteButton.innerHTML = "Delete";
-    tdElement = document.createElement("td");
-    tdElement.appendChild(deleteButton);
-    tableRowElement.appendChild(tdElement);
-  }
-}
-
-function destroyAndRecreateTable() {
-  // destroy table
-  let tableBodyElement = document.querySelector("#table_body");
-  tableBodyElement.remove();
-  // create brand new table (entire table)
-  let responseData = getData();
-  responseData.then(dataRowsArray => {
-    createTable(dataRowsArray);
-  });
-}
-
-function checkRowForUpdates(childTDElements) {
-  let updates = false;
-  for (let rowIndex = 1; rowIndex < childTDElements.length - 2; rowIndex++) {
-    let classStr = childTDElements[rowIndex].firstChild.getAttribute("class");
-    if (classStr) {
-      if (classStr.includes("field-updated")) {
-        updates = true;
-        break;
-      }
-    }
-  }
-  return updates;
-}
-
-function deleteButtonListener() {
-  let tableElement = document.querySelector("#many-to-many-table");
-  
-  tableElement.addEventListener("click", event => {
-    let targetElement = event.target;
-
-    // update button behavior
-    let buttonClass = targetElement.getAttribute("class");
-    if(buttonClass === "delete-button") {
-      let trElement = targetElement.parentElement.parentElement;
-      let TDElement = trElement.firstChild;
-      let muscle_group_id = TDElement.innerText;
-      TDElement = TDElement.nextElementSibling;
-      let exercise_id = TDElement.innerText;
-      
-      // send delete command to server via API
-      let response = deleteRow(muscle_group_id, exercise_id);
-      response.then(success => {
-        if (success) {
-          destroyAndRecreateTable();
-        }
-      });
-    }
-  });
-};
-    
-function updateButtonListener() {
-  let tableElement = document.querySelector("#many-to-many-table");
-  
-  tableElement.addEventListener("click", event => {
-    let targetElement = event.target;
-
-    // update button behavior
-    let buttonClass = targetElement.getAttribute("class");
-    if(buttonClass === "update-button") {
-      let trElement = targetElement.parentElement.parentElement;
-      let childTDElements = trElement.children;
-
-      // check if anything was changed before starting update process
-      let updates = checkRowForUpdates(childTDElements);
-      if (updates) {
-        // update API call here
-        updateButtonAPICall(childTDElements);
-      }
-    }
-});
-}
-
-function editButtonListener() {
-  let tableElement = document.querySelector("#many-to-many-table");
-  
-  // undisable row when user clicks Edit button
-  tableElement.addEventListener("click", event => {
-    let targetElement = event.target;
-    let buttonClass = targetElement.getAttribute("class");
-    if(buttonClass === "edit-button") {
-      let trElement = targetElement.parentElement.parentElement;
-      let childTDElements = trElement.children;
-
-      for (let rowIndex = 1; rowIndex < childTDElements.length - 2; rowIndex++) {
-        childTDElements[rowIndex].firstChild.disabled = false;
-      }
-      targetElement.innerText = "Update";
-      targetElement.className = "update-button";
-      }
-  });
-}
-
-function updateTableDataInputValues() {
-  let tableElement = document.querySelector("#many-to-many-table");
-  tableElement.addEventListener("input", event => {
-    event.target.setAttribute("value", event.target.value);
-    let classes = event.target.classList;
-    classes.add("field-updated");
-  });
+function apiCalls() {
+  this.getData = getData;
+  this.addRow = addRow;
+  this.updateData = updateData;
+  this.deleteRow = deleteRow;
+  this.getMuscleGroupsData = getMuscleGroupsData;
+  this.getExercisesData = getExercisesData;
 };
 
-function addButtonSendAPICall() {
+function addButtonSendAPICall(apiCallsObj, dataAttributes, idArray) {
   let addButton = document.querySelector("#mtm-add-button");
   addButton.addEventListener("click", () => {
     let selectElement = document.querySelector("#muscle-group-select");
@@ -334,35 +199,28 @@ function addButtonSendAPICall() {
     };
 
     // sending data to flask server via API
-    let response = addRow(data);
+    let response = apiCallsObj.addRow(data);
     response.then(success => {
       if (success) {
         // clear data in select element after Add button clicked
-        resetSelectElements();
-        destroyAndRecreateTable();
+        resetSelectElement("#muscle-group-select");
+        resetSelectElement("#exercise-select");
+        destroyAndRecreateTable(apiCallsObj, dataAttributes, idArray);
       }
     });
   });
 };
 
-function createEventListeners() {
+function createEventListeners(idArray) {
   // update 'value' attribute when user changes input in table
-  updateTableDataInputValues();
+  updateTableDataInputValuesListener(TABLE_ID);
 
   // event listeners for clicks on update and delete buttons
-  // editButtonListener();
-  // updateButtonListener();
-  deleteButtonListener();
+  let apiCallsObj = new apiCalls();
+  deleteButtonListenerManyToMany(apiCallsObj, dataAttributes, idArray);
 
   // event listener for adding new exercise via form
-  addButtonSendAPICall();
-};
-
-function clearFormInputs() {
-  let formElement = document.querySelector("#form");
-  for (let index = 0; index < formElement.length - 1; index++) {
-    formElement[index].value = "";
-  }
+  addButtonSendAPICall(apiCallsObj, dataAttributes, idArray);
 };
 
 function completeMuscleGroupsSelectOptions(dataRowsArray) {
@@ -376,35 +234,15 @@ function completeMuscleGroupsSelectOptions(dataRowsArray) {
   }
 };
 
-function completeExercisesSelectOptions(dataRowsArray) {
-  let exerciseSelectElement = document.querySelector("#exercise-select");
-  for (let rowIndex = 0; rowIndex < dataRowsArray.length; rowIndex++) {
-    let exercise_data = dataRowsArray[rowIndex];
-    let optionElement = document.createElement("option");
-    let exerciseID = exercise_data.exercise_id;
-    optionElement.setAttribute("value", exerciseID);
-    let text = "";
-    let keys = Object.keys(exercise_data);
-    for (let i = 0; i < keys.length - 1; i++) {
-      text += exercise_data[keys[i]] + ", ";
-    }
-    text += exercise_data["equipment_name"];
-    optionElement.innerText = text;
-    exerciseSelectElement.appendChild(optionElement);
-  }
-};
-
-function resetSelectElements() {
-  let selectElement = document.querySelector("#muscle-group-select");
-  selectElement.selectedIndex = 0;
-  selectElement = document.querySelector("#exercise-select");
-  selectElement.selectedIndex = 0;
-};
-
 function main() {
   let responseData = getData();
+  let idArray = ["muscle_group_id", "exercise_id"];
   responseData.then(dataRowsArray => {
-    createTable(dataRowsArray);
+    createManyToManyTable(
+      dataRowsArray,
+      dataAttributes,
+      idArray
+    );
   });
   
   let muscleGroupsData = getMuscleGroupsData();
@@ -417,8 +255,9 @@ function main() {
     completeExercisesSelectOptions(exercisesDataArray);
   });
   
-  resetSelectElements();
-  createEventListeners();
+  resetSelectElement("#muscle-group-select");
+  resetSelectElement("#exercise-select");
+  createEventListeners(idArray);
 };
 
 main();
